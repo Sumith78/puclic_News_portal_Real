fdbfb7b4f08b3a6fba6540f23000ced5d00a868e
sumith kumar singh <sumithrajput53361@gmail.com>
2023-05-25 10:27:16 +0530

    commit

---
+++ src/java/Logic/path.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Sumit
+ */
+public class path {
+    public static String p="C:/Users/sumit/Music/Public_news_portal/Public_news_portal/web/";
+    public static String name="Public News Portal";
+}
